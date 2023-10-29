ce72d56bbbddd644bc702a89717548699f15270d
Artur <arthurlavrin@gmail.com>
2023-10-29 16:47:00 +0200

    Task5-Integration_with_S3

---
+++ import-service/serverless.ts
@@ -0,0 +1,57 @@
+import type { AWS } from "@serverless/typescript";
+
+import { importProductsFile, importFileParser } from "src/functions";
+
+const serverlessConfiguration: AWS = {
+  service: "import-service",
+  frameworkVersion: "3",
+  plugins: ["serverless-esbuild", "serverless-offline", 'serverless-auto-swagger', 'serverless-webpack'],
+  provider: {
+    name: "aws",
+    runtime: "nodejs18.x",
+    httpApi: {
+      cors: true,
+    },
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    region: "eu-west-1",
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+      NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
+    },
+
+    iam: {
+      role: {
+        statements: [
+          {
+            Effect: "Allow",
+            Action: ["s3:*"],
+            Resource: ["arn:aws:s3:::uploaded-artlav/*"],
+          },
+        ],
+      },
+    },
+  },
+
+  functions: { importProductsFile, importFileParser },
+  package: { individually: true },
+  custom: {
+    esbuild: {
+      bundle: true,
+      minify: false,
+      sourcemap: true,
+      exclude: ["aws-sdk"],
+      target: "node14",
+      define: { "require.resolve": undefined },
+      platform: "node",
+      concurrency: 10,
+    },
+    webpack: {
+      excludeFiles: '**/*.spec.ts'
+    },
+  },
+};
+
+module.exports = serverlessConfiguration;
---
+++ import-service/src/functions/importFileParser/handler.ts
@@ -0,0 +1,71 @@
+import {
+  CopyObjectCommand,
+  DeleteObjectCommand,
+  GetObjectCommand,
+  S3Client,
+} from "@aws-sdk/client-s3";
+import { S3Event } from "aws-lambda";
+import csvParser from "csv-parser";
+
+const getCopyCommand = (key: string) =>
+  new CopyObjectCommand({
+    Bucket: "uploaded-artlav",
+    CopySource: `uploaded-artlav/${key}`,
+    Key: key.replace("uploaded", "parsed"),
+  });
+
+const getDeleteCommand = (key: string) =>
+  new DeleteObjectCommand({
+    Bucket: "uploaded-artlav",
+    Key: key,
+  });
+
+const importFileParser = async (event: S3Event) => {
+  try {
+    await Promise.all(
+      event.Records.map(async (record) => {
+        const s3Client = new S3Client({ region: "eu-west-1" });
+        const stream = (
+          await s3Client.send(
+            new GetObjectCommand({
+              Bucket: "uploaded-artlav",
+              Key: record.s3.object.key,
+            })
+          )
+        ).Body;
+
+        return new Promise((resolve, reject) => {
+          stream
+          .pipe(csvParser())
+          .on("data", (data) => {
+            console.log("CSV file is being processed: ", data);
+          })
+          .on("error", (error) => {
+            console.error("Error when parsed the file ", error);
+            reject(error);
+          })
+          .on("end", async () => {
+            console.log("Parsing of data is finished");
+
+            await Promise.all([
+              s3Client.send(getCopyCommand(record.s3.object.key)),
+              s3Client.send(getDeleteCommand(record.s3.object.key)),
+            ]);
+            console.log(
+              "the file has been copied to parsed folder and removed from uploaded folder"
+            );
+            resolve("success");
+          });
+        });
+      })
+    );
+
+    return {
+      message: "success",
+    };
+  } catch (error) {
+    return { error };
+  }
+};
+
+export const main = importFileParser;
